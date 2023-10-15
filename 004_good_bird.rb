e3e9bf638c6ba93cd465265a0cda6095d3d1cff8
kazvee <kayvee123@gmail.com>
2023-10-14 23:13:57 -0600

    Add simple example of the inspect built-in Ruby instance method

--- 004_good_bird.rb
+++ 004_good_bird.rb
@@ -17,3 +17,7 @@ end
 tweety = GoodBird.new("Tweety", 4)
 puts tweety.age             # => 8
 puts tweety                 # => This bird's name is Tweety and it is 8 in bird years.
+
+# The inspect method is very helpful for debugging purposes
+# The equivalent of `puts tweety.inspect` is:
+p tweety                    # => #<GoodBird:0x000055d4bfa555e8 @name="Tweety", @age=8>
