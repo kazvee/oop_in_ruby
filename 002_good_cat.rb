b4163ea06384316a06a8ad401566c1d233675554
kazvee <kayvee123@gmail.com>
2023-10-14 19:36:11 -0600

    Add get_name method to GoodCat class to access cat names in output

--- 002_good_cat.rb
+++ 002_good_cat.rb
@@ -3,6 +3,10 @@ class GoodCat
     @name = name
   end
 
+  def get_name
+    @name
+  end
+
   def speak
     "#{@name} says Meow!"
   end
@@ -10,6 +14,8 @@ end
 
 mittens = GoodCat.new("Mittens")
 puts mittens.speak                # => Mittens says Meow!
+puts mittens.get_name
 
 muffy = GoodCat.new("Muffy")
 puts muffy.speak                  # => Muffy says Meow!
+puts muffy.get_name
