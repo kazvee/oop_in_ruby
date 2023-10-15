2fb7d7aa2129ffd065d2db48ba8b0135d6c46b11
kazvee <kayvee123@gmail.com>
2023-10-14 19:12:36 -0600

    Add instance method of speak to GoodCat class

--- 002_good_cat.rb
+++ 002_good_cat.rb
@@ -2,6 +2,11 @@ class GoodCat
   def initialize(name)
     @name = name
   end
+
+  def speak
+    "Meow!"
+  end
 end
 
 mittens = GoodCat.new("Mittens")
+puts mittens.speak                # => Meow!
