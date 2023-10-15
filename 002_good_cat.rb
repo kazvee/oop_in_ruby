0034063dca6e63fb93e7ba51b386bd7bfcfa893f
kazvee <kayvee123@gmail.com>
2023-10-14 21:57:01 -0600

    Refactor GoodCat class to use attr_accessor method for name property

--- 002_good_cat.rb
+++ 002_good_cat.rb
@@ -1,27 +1,37 @@
 class GoodCat
+
+  # The `attr_accessor` method takes a symbol as an argument, and uses it to create the method name for the getter and setter methods
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
 
-  def name
-    @name
-  end
+  # Replaced by `attr_accessor` method
+  # If we only want the getter without the setter, we can use the `attr_reader` method
 
-  def name=(n)
-    @name = n
-  end
+  # def name
+  #   @name
+  # end
+
+  # Replaced by `attr_accessor` method
+  # If we only want the setter without the getter, we can use the `attr_writer` method 
+
+  # def name=(n)
+  #   @name = n
+  # end
 
   def speak
-    "#{@name} says Meow!"
+    "#{name} says Meow!"
   end
 end
 
 mittens = GoodCat.new("Mittens")
-puts mittens.speak                # => Mittens says Meow!
+puts mittens.speak              
 puts mittens.name
 mittens.name = "Miss Mittens"
 puts mittens.name
 
 muffy = GoodCat.new("Muffy")
-puts muffy.speak                  # => Muffy says Meow!
+puts muffy.speak              
 puts muffy.name
