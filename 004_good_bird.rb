89f950e0fa9e6e19c9b0f0e593174b84a200509d
kazvee <kayvee123@gmail.com>
2023-10-14 23:10:47 -0600

    Add custom to_s method to GoodBird to override Ruby default method

--- 004_good_bird.rb
+++ 004_good_bird.rb
@@ -7,7 +7,13 @@ class GoodBird
     self.name = n
     self.age  = a * BIRD_YEARS
   end
+
+  def to_s
+    "This bird's name is #{name} and it is #{age} in bird years."
+  end
+
 end
 
 tweety = GoodBird.new("Tweety", 4)
 puts tweety.age             # => 8
+puts tweety                 # => This bird's name is Tweety and it is 8 in bird years.
