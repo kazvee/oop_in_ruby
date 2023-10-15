39c61507ba8d77dfc45c109679943626a9cb71a2
kazvee <kayvee123@gmail.com>
2023-10-14 19:39:06 -0600

    Add set_name method to GoodCat class to amend cat names

--- 002_good_cat.rb
+++ 002_good_cat.rb
@@ -7,6 +7,10 @@ class GoodCat
     @name
   end
 
+  def set_name=(name)
+    @name = name
+  end
+
   def speak
     "#{@name} says Meow!"
   end
@@ -14,6 +18,8 @@ end
 
 mittens = GoodCat.new("Mittens")
 puts mittens.speak                # => Mittens says Meow!
+puts mittens.get_name
+mittens.set_name = "Miss Mittens"
 puts mittens.get_name
 
 muffy = GoodCat.new("Muffy")
