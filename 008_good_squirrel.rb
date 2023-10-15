a1588d074a59a406c9d9fc57991ed57d632178ad
kazvee <kayvee123@gmail.com>
2023-10-15 13:17:51 -0600

    Add example class to show importance of module order in method lookup

--- 008_good_squirrel.rb
+++ 008_good_squirrel.rb
@@ -24,6 +24,11 @@ class Animal
   end
 end
 
+class GoodSquirrel < Animal
+  include Swimmable         # <= Added FIRST
+  include Climbable         # <= Added LAST
+end
+
 puts "---Animal method lookup---"
 puts Animal.ancestors
 
@@ -37,4 +42,23 @@ puts Animal.ancestors
 fluffy = Animal.new
 puts fluffy.speak           # => I'm an animal, and I speak!
 puts fluffy.walk            # => I'm walking.
-puts fluffy.swim            # => NoMethodError: undefined method `swim' for #<Animal:0x000056068ceb1890>
+# puts fluffy.swim          # => NoMethodError: undefined method `swim' for #<Animal:0x000056068ceb1890>
+
+puts "---GoodSquirrel method lookup---"
+puts GoodSquirrel.ancestors
+
+# ---GoodSquirrel method lookup---
+# GoodSquirrel
+# Climbable                 <= Added LAST
+# Swimmable                 <= Added FIRST
+# Animal
+# Walkable
+# Object
+# Kernel
+# BasicObject
+
+# The order in which we include modules is important: Ruby looks at the LAST module we included FIRST
+# So in the rare occurrence that the modules we mix in contain a method with the same name, the LAST module included will be consulted FIRST 
+# Notice also that the module included in the superclass made it on to the method lookup path 
+# This shows that all GoodSquirrel objects will have access to not only Animal methods, but also methods defined in the Walkable module, 
+# as well as all other modules mixed in to any of its superclasses
