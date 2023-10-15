d298b239367f21055ea97f721a47af692824198c
kazvee <kayvee123@gmail.com>
2023-10-15 14:16:37 -0600

    Add example using modules as a container for methods (module methods)

--- 009_good_moose.rb
+++ 009_good_moose.rb
@@ -10,9 +10,17 @@ module Mammal
       p "#{name}"
     end
   end
+
+  def self.some_out_of_place_method(num)
+    num ** 2
+  end
 end
 
 leo = Mammal::Lion.new
 morty = Mammal::Moose.new
 leo.speak('Raar!')           # => "Raar!"
 morty.say_name('Morty')      # => "Morty"
+
+puts value = Mammal.some_out_of_place_method(4)
+# Can also call the method like this, but it is not the preferred way:
+# puts value = Mammal::some_out_of_place_method(4)
